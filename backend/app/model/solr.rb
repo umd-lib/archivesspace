4a36ae116b54c03db603ef563486a2183ae35d1f
Laney McGlohon <lmcglohon@gmail.com>
2018-06-20 15:10:32 -0700

    Add range search back in and update configuration parameters

--- backend/app/model/solr.rb
+++ backend/app/model/solr.rb
@@ -235,8 +235,21 @@ class Solr
       if @show_published_only
         add_solr_param(:fq, "publish:true")
         add_solr_param(:fq, "types:pui")
+        if AppConfig[:solr_params].any?
+          if !AppConfig[:solr_params].has_key? :qf
+            add_solr_param(:qf, pui_qf)
+          end
+        else
+          add_solr_param(:qf, pui_qf)
+        end
       else
-        add_solr_param(:qf, "fullrecord")
+        if AppConfig[:solr_params].any?
+          if !AppConfig[:solr_params].has_key? :qf
+            add_solr_param(:qf, staff_ui_qf)
+          end
+        else
+          add_solr_param(:qf, staff_ui_qf)
+        end
       end
 
 
@@ -260,6 +273,25 @@ class Solr
 
       if @query_type == :edismax
         add_solr_param(:defType, "edismax")
+        if AppConfig[:solr_params].any?
+          if !AppConfig[:solr_params].has_key? :"q.op"
+            add_solr_param(:"q.op", "AND")
+          end
+          if !AppConfig[:solr_params].has_key? :mm
+            add_solr_param(:mm, "6<-1 6<90%")
+          end
+          if !AppConfig[:solr_params].has_key? :pf
+            add_solr_param(:pf, "four_part_id^50")
+          end
+          if !AppConfig[:solr_params].has_key? :bq
+            add_solr_param(:bq, "primary_type:resource^100 primary_type:accession^100 primary_type:subject^50 primary_type:agent_person^50 primary_type:agent_corporate_entity^30 primary_type:agent_family^30")
+          end
+        else
+          add_solr_param(:"q.op", "AND")
+          add_solr_param(:mm, "6<-1 6<90%")
+          add_solr_param(:pf, "four_part_id^50")
+          add_solr_param(:bq, "primary_type:resource^100 primary_type:accession^100 primary_type:subject^50 primary_type:agent_person^50 primary_type:agent_corporate_entity^30 primary_type:agent_family^30")
+        end
       end
 
       # do it here so instance variables can be resolved
