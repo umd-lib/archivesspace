00196615851f9ed9b5bf65f9f86d059d31fb6ce8
Laney McGlohon <lmcglohon@gmail.com>
2018-07-06 09:37:18 -0700

    Remove unecessary code from welcome controller Correct spec file names

--- public/app/controllers/welcome_controller.rb
+++ public/app/controllers/welcome_controller.rb
@@ -1,7 +1,6 @@
 class WelcomeController < ApplicationController
   def show
     @page_title = I18n.t 'brand.welcome_page_title'
-    @search = Search.new(params)
-    render  
+    render
   end
 end
